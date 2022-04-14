ce50293e89ac5fa6924dbeccc1da562f85f29af8
mw <mw@macbook-air-von-marc>
2022-04-14 17:03:14 +0200

    Changed ATM Accounts and ATM balance

--- src/idh/java/ATM.java
+++ src/idh/java/ATM.java
@@ -2,9 +2,12 @@ package idh.java;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.*;
 
 public class ATM {
-	int accountBalance = 100;
+	int[] accountBalance = {100,20,40}; //geld position wie users aufgeteilt.
+	int balanceATM = 90;
 
 	/**
 	 * Main command loop of the ATM Asks the user to enter a number, and passes this
@@ -12,27 +15,43 @@ public class ATM {
 	 * produces money. If the user enters anything else than an integer number, the
 	 * loop breaks and the program exists
 	 */
-	public void run() {
+	public void run(List<Integer> users) {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int found = 0;
 		while (true) {
 			try {
+				System.out.print("Enter your account number: ");
+				int user = Integer.parseInt(br.readLine());
+				
+				if(users.indexOf(user) == -1) {
+					System.out.println("Falscher Account!");
+					run(users);
+				}else {
+					found = users.indexOf(user);; //ändern für position
+				}
+				
+					
 				System.out.print("Enter the amount to withdraw: ");
 				int amount = Integer.parseInt(br.readLine());
-				cashout(amount);
+				cashout(amount, found);
 			} catch (Exception e) {
 				break;
 			}
 		}
 	}
 
-	public void cashout(int amount) {
-		if (amount < accountBalance) {
-			accountBalance = accountBalance - amount;
-			System.out.println("Ok, here is your money, enjoy!");
-		} else {
-			System.out.println("Sorry, not enough money in the bank.");
+	public void cashout(int amount, int person) {
+		if(amount > balanceATM) {
+			System.out.println("Sorry, the ATM does't have that much cash anymore.");
+		}else {
+			if (amount < accountBalance[person]) {
+				accountBalance[person] = accountBalance[person] - amount;
+				balanceATM -= amount;
+				System.out.println("Ok, here is your money, enjoy!");
+			} else {
+				System.out.println("Sorry, you don't have enough money in the bank.");
+			}
 		}
-
 	};
 
 	/**
@@ -40,7 +59,13 @@ public class ATM {
 	 */
 	public static void main(String[] args) {
 		ATM atm = new ATM();
-		atm.run();
+		
+		List<Integer> users = new ArrayList<Integer>(3); //könnte auch über eine Klasse mit dem kontostand zusammen in einer Klasse implementiert werden.
+		users.add(123);
+		users.add(234);
+		users.add(345);
+		
+		atm.run(users);
 	};
 
 }
