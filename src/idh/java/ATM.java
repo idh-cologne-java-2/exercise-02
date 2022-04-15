61e75c86fb83309290d59587e06aace1b07579a2
Leon <Leon@margale.de>
2022-04-15 21:26:12 +0200

    Java nr Lösung

--- src/idh/java/ATM.java
+++ src/idh/java/ATM.java
@@ -3,8 +3,13 @@ package idh.java;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 
+
 public class ATM {
-	int accountBalance = 100;
+	
+	private int ATMBalance = 2000; 
+	private Konten N = new Konten(1234, "Niklas Brudde", 500); 
+	private Konten P = new Konten(4321, "john Locke" , 700 );
+	private Konten S = new Konten(5678, "Silvia gebrüll", 1300); 
 
 	/**
 	 * Main command loop of the ATM Asks the user to enter a number, and passes this
@@ -13,24 +18,59 @@ public class ATM {
 	 * loop breaks and the program exists
 	 */
 	public void run() {
+		
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		while (true) {
 			try {
+				System.out.print(" Please Enter your Account ID:  "); 
+				int currentID = Integer.parseInt(br.readLine());
+				if(currentID != N.ID && currentID != P.ID && currentID != S.ID) {
+					System.err.println("\n Sorry Wrong ID please Try again .. ");
+					run(); 
+				}
+					
 				System.out.print("Enter the amount to withdraw: ");
 				int amount = Integer.parseInt(br.readLine());
-				cashout(amount);
+				cashout(amount , currentID);
 			} catch (Exception e) {
 				break;
 			}
 		}
 	}
 
-	public void cashout(int amount) {
-		if (amount < accountBalance) {
-			accountBalance = accountBalance - amount;
-			System.out.println("Ok, here is your money, enjoy!");
+	public void cashout(int amount , int currentID) {
+		if( amount > ATMBalance) {
+			System.err.print("Sorry there is not enough money in the ATM"); 
 		} else {
-			System.out.println("Sorry, not enough money in the bank.");
+			switch(currentID) {
+			case 1234: 
+				if (amount > N.MoneyBalance) {
+					System.err.print("Sorry you dont habe enough Money in your Bank account \n");
+					break ; 
+				}
+				N.MoneyBalance -= amount; 
+				ATMBalance -= amount ; 
+				System.out.print("crazy man you cashed out " + amount + " you have "+ N.MoneyBalance + " $ left. \n");
+				break ; 
+			case 4321: 
+				if (amount > P.MoneyBalance) {
+					System.err.print("Sorry you dont habe enough Money in your Bank account \n");
+					break ; 
+				}
+				P.MoneyBalance -= amount; 
+				ATMBalance -= amount ; 
+				System.out.print("crazy man you cashed out " + amount + " you have "+ P.MoneyBalance + " $ left. \n");
+				break ; 
+			case 5678: 
+				if (amount > S.MoneyBalance) {
+					System.err.print("Sorry you dont habe enough Money in your Bank account \n");
+					break ; 
+				}
+				S.MoneyBalance -= amount; 
+				ATMBalance -= amount ; 
+				System.out.print("crazy man you cashed out " + amount + " you have "+ S.MoneyBalance + " $ left. \n");
+				break ; 
+			}
 		}
 
 	};
---
+++ src/idh/java/Konten.java
@@ -0,0 +1,19 @@
+package idh.java;
+
+
+public class Konten {
+	
+	
+	int ID ; 
+	 String Name ; 
+	 int MoneyBalance; 
+	
+	public Konten( int ID, String Name , int MoneyBalance) {
+		this.ID = ID ; 
+		this.Name = Name ; 
+		this.MoneyBalance = MoneyBalance ; 
+	}
+	
+	
+
+}
