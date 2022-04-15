2be48b87b600ce89f03854e20ddc435911f09f23
JRadermacher <jana.radermacher7@gmx.de>
2022-04-15 19:16:35 +0200

    Objektorientierte Lösung mit Kommentaren

--- src/main/java/idh/java/ATM.java
+++ src/main/java/idh/java/ATM.java
@@ -6,6 +6,7 @@ import java.util.Random;
 
 public class ATM {
 
+	//Summe, die auf ATM vorhanden ist
 	int geldATM = 100000;
 
 
@@ -17,21 +18,25 @@ public class ATM {
 				System.out.print("Gib bitte zuerst deine Kontonummer an: ");
 				String kontoNummerString = br.readLine();
 
-
+				//Prüfen, ob User "exit" eingetippt hat
 				if (kontoNummerString.equalsIgnoreCase("exit")){
 					System.out.println("Vorgang wird beendet, Bis bald");
 					break;
 				}
 
+				//Parsen des Strings in Integer
 				int kontoNr = Integer.parseInt(kontoNummerString);
 
+				//Prüfen ob Konto (Funktion "kontoExistiert") existiert
 				if (!Konto.kontoExistiert(kontoNr)){
 					System.out.println("Angegebene KontoNr. existiert nicht (zulässige Nr. 100-200)");
 
 					continue;
 				}
 
+				//schaue in HashMap nach ob entsprechender Eintrag existiert
 				Konto k = Konto.getKonto(kontoNr);
+
 				System.out.println("Dein aktueller Kontostand: " + k.getKontoStand() + " €");
 
 				System.out.print("Gib den Betrag an der abgehoben werden soll: ");
@@ -42,10 +47,9 @@ public class ATM {
 					break;
 				}
 
+				int betragAbbuchung = Integer.parseInt(betragString);
 
-				int amount = Integer.parseInt(betragString);
-
-				cashout(amount, k);
+				cashout(betragAbbuchung, k);
 
 			}
 			catch (Exception e) {
@@ -53,12 +57,12 @@ public class ATM {
 				break;
 
 			}
-
 		}
-
-
 	}
 
+	//Initialisierung
+	//Erstellen von random Zahlen (Kontonr. zw. 100 und 200)
+	//Nachkommastellen (float) auf 2 reduzieren
 	public void init () {
 		Random random = new Random();
 
@@ -74,25 +78,25 @@ public class ATM {
 	}
 
 
-	public void cashout(int amount, Konto k) {
+	public void cashout(int betrag, Konto k) {
 
-		if (amount <= 0) {
+		if (betrag <= 0) {
 			System.out.println("Negativer Betrag wird nicht akzeptiert");
 			return;
 		}
 
-		if (amount % 5 != 0) {
+		if (betrag % 5 != 0) {
 			System.out.println("Betrag kann nicht ausgegeben werden, da keine Münzen vorhanden sind");
-			System.out.println("Mögliche Beträge sind: " + amount / 5 * 5 + " oder " + ((amount / 5) + 1) * 5 + " Euro");
+			System.out.println("Mögliche Beträge sind: " + betrag / 5 * 5 + " oder " + ((betrag / 5) + 1) * 5 + " Euro");
 			return;
 		}
 
-		if (amount > geldATM) {
+		if (betrag > geldATM) {
 			System.out.println("Dieser Automat hat zu wenig Geld!");
 		} else {
-			boolean success = k.geldAbheben(amount);
+			boolean success = k.geldAbheben(betrag);
 			if (success) {
-				geldATM -= amount;
+				geldATM -= betrag;
 			}
 		}
 	}
--- src/main/java/idh/java/Konto.java
+++ src/main/java/idh/java/Konto.java
@@ -5,32 +5,45 @@ import java.util.HashMap;
 public class Konto {
 
    // final private String inhaber;
+
+    //kontoStand des Inhabers als float
     private float kontoStand;
+
+    //HashMap mit Key (Kontonummer) und Value (Konto bzw. Kontostand)
     private static final HashMap<Integer,Konto> konten = new HashMap<>();
 
+    //Konstruktor
     public Konto(float kontoStand, int kontoNummer) {
         this.kontoStand = kontoStand;
 
+        //Befüllen der HashMap mit kontoNummer und Konto (this)
         konten.put(kontoNummer,this);
     }
 
+    //GETTER getKontostand
     public float getKontoStand() {
         return kontoStand;
     }
 
+    //Prüfen ob Kontonummer existiert
+    //Wenn keine existiert - Exception werfen, ansonsten
     public static Konto getKonto (int kontoNummer) throws Exception {
         if (!kontoExistiert(kontoNummer)){
             throw new Exception ("Konto gibt es nicht!");
 
         }
+        //extrahiere Konto aus HashMap
         return konten.get(kontoNummer);
     }
 
+    //Funktion, ob Konto mit angegebener KontoNr. existiert
     public static boolean kontoExistiert (int kontoNummer) {
+
         return konten.containsKey(kontoNummer);
     }
 
 
+    //geldAbheben erfolgreich ja oder nein
     public boolean geldAbheben (int betrag){
         if (betrag > this.kontoStand){
             System.out.println("Du hast nicht genug Geld auf deinem Konto!");
