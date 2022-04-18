cf1e7cdea55b83fc7118c62436abbdb178829231
UntoastedToast <UntoastedToast@users.noreply.github.com>
2022-04-18 16:25:00 +0200

    Homework completed

--- src/idh/java/ATM.java
+++ src/idh/java/ATM.java
@@ -4,33 +4,56 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 
 public class ATM {
-	int accountBalance = 100;
+	int cash = 1000;
+
+	int[] accountNumber = new int[] { 1122, 2233, 4455, 6677 };
+	int[] accountBalance = new int[] { 150, 200, 400, 1000 };
+
 
 	/**
-	 * Main command loop of the ATM Asks the user to enter a number, and passes this
-	 * number to the function cashout(...) which actually does the calculation and
-	 * produces money. If the user enters anything else than an integer number, the
-	 * loop breaks and the program exists
+	 * Main command loop of the ATM Asks the user to enter an account number and
+	 * then a number, and passes this number to the function cashout(...) which
+	 * actually does the calculation and produces money. If the user enters anything
+	 * else than an integer number, the loop breaks and the program exists
 	 */
 	public void run() {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		System.out.println("The following account numbers exist: 1122, 2233, 4455, 6677");
+		
 		while (true) {
 			try {
-				System.out.print("Enter the amount to withdraw: ");
-				int amount = Integer.parseInt(br.readLine());
-				cashout(amount);
+				System.out.print("Enter your account number: ");
+				int number = Integer.parseInt(br.readLine());
+				int index = -1;
+				for (int i = 0; i < accountNumber.length; i++) {
+					if (number == accountNumber[i]) {
+						index = i;
+					}
+				}
+				if (index != -1) {
+					System.out.print("Enter the amount to withdraw: ");
+					int amount = Integer.parseInt(br.readLine());
+					cashout(index, amount);
+				}
 			} catch (Exception e) {
 				break;
 			}
 		}
 	}
 
-	public void cashout(int amount) {
-		if (amount < accountBalance) {
-			accountBalance = accountBalance - amount;
-			System.out.println("Ok, here is your money, enjoy!");
+	public void cashout(int index, int amount) {
+
+		if (cash >= amount) {
+			if (amount <= accountBalance[index]) {
+				accountBalance[index] = accountBalance[index] - amount;
+				cash = cash - amount;
+				System.out.println("Ok, here you go!");
+			} else {
+				System.out.println("Sorry, you don't have enough money in the bank.");
+			}
 		} else {
-			System.out.println("Sorry, not enough money in the bank.");
+			System.out.println("Sorry, the ATM doesn't have that much cash anymore.");
 		}
 
 	};
@@ -42,5 +65,4 @@ public class ATM {
 		ATM atm = new ATM();
 		atm.run();
 	};
-
 }
