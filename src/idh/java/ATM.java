11bb99513f0751952335aa5d744a535c8c0568af
Admin <Admin@Jasmin-Laptop>
2022-04-18 13:23:21 +0200

    Änderung der Musterlösung zur exercise-01

--- src/idh/java/ATM.java
+++ src/idh/java/ATM.java
@@ -26,12 +26,13 @@ public class ATM {
 	}
 
 	public void cashout(int amount) {
-		if (amount < accountBalance) {
+		if (amount <= accountBalance) {
 			accountBalance = accountBalance - amount;
 			System.out.println("Ok, here is your money, enjoy!");
 		} else {
 			System.out.println("Sorry, not enough money in the bank.");
 		}
+		
 
 	};
 
