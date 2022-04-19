7b9f4af28ec941c07e88bb169496b002e629a83b
Marius Krämer <103112167+RealMajus@users.noreply.github.com>
2022-04-19 20:21:54 +0200

    Test

--- src/idh/java/ATM.java
+++ src/idh/java/ATM.java
@@ -1,22 +1,26 @@
-package idh.java;
+package b;
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
+import java.io.*;
 
 public class ATM {
-	int accountBalance = 100;
-
+	   int zaster = 69;
+	   int geldspeicher = 50;
+	   
+	   int kontonr[] = 123, 345, 567, 789;
+	   int 
+	   
 	/**
-	 * Main command loop of the ATM Asks the user to enter a number, and passes this
-	 * number to the function cashout(...) which actually does the calculation and
-	 * produces money. If the user enters anything else than an integer number, the
-	 * loop breaks and the program exists
+	 * Main command loop of the ATM
+	 * Asks the user to enter a number, and passes this number to the function cashout(...) 
+	 * which actually does the calculation and produces money.
+	 * If the user enters anything else than an integer number, the loop breaks 
+	 * and the program exists
 	 */
 	public void run() {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		while (true) {
+		while(true) {
 			try {
-				System.out.print("Enter the amount to withdraw: ");
+				System.out.print("Geben sie hier den gewünschten Betrag ein: ");
 				int amount = Integer.parseInt(br.readLine());
 				cashout(amount);
 			} catch (Exception e) {
@@ -25,16 +29,37 @@ public class ATM {
 		}
 	}
 
+	
 	public void cashout(int amount) {
-		if (amount < accountBalance) {
-			accountBalance = accountBalance - amount;
-			System.out.println("Ok, here is your money, enjoy!");
-		} else {
-			System.out.println("Sorry, not enough money in the bank.");
+		
+		if ( geldspeicher >= amount ) {
+			
+			if (amount <= zaster) {
+			zaster -= amount;
+			geldspeicher -= amount;
+			System.out.println("Sie haben "+ amount+ "€ abgehoben");
+			System.out.println("Ihr Kontostand beträgt jetzt "+ zaster + "€");
+					} 
+		
+		
+		else if (amount > zaster){
+			System.out.println("Ihr Kontostand ist zu niedrig um "+ amount + "€ auszugeben");
+			System.out.println("Ihr Kontostand beträgt "+ zaster + "€");
 		}
-
-	};
-
+		
+	
+		/*
+	 	else {
+			System.out.println("Ihr Kontostand ist zu niedrig um "+ amount + "€ auszugeben");
+			System.out.println("Ihr Kontostand beträgt "+ zaster + "€");
+		}*/
+	}
+	
+		else {
+			System.out.println("Dieser Geldautomat verfügt nicht über die nötigen Kapazitäten");
+		}
+	}
+	
 	/**
 	 * Launches the ATM
 	 */
@@ -42,5 +67,4 @@ public class ATM {
 		ATM atm = new ATM();
 		atm.run();
 	};
-
-}
+};
