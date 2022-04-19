f7f646b2534e0707215214781fe8cbc3fa371939
Alessandro M <sando1199@web.de>
2022-04-19 13:27:39 +0200

    Task solved

--- src/idh/java/ATM.java
+++ src/idh/java/ATM.java
@@ -4,8 +4,13 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 
 public class ATM {
-	int accountBalance = 100;
-
+	int ATMBalance = 70;
+	
+	People hans = new People(123, 50);
+	People richard = new People(234, 50);
+	People susy = new People(345, 70);
+	People[] peopleList = {hans, richard, susy};
+	
 	/**
 	 * Main command loop of the ATM Asks the user to enter a number, and passes this
 	 * number to the function cashout(...) which actually does the calculation and
@@ -13,24 +18,40 @@ public class ATM {
 	 * loop breaks and the program exists
 	 */
 	public void run() {
+		
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		while (true) {
+			
 			try {
-				System.out.print("Enter the amount to withdraw: ");
-				int amount = Integer.parseInt(br.readLine());
-				cashout(amount);
+				System.out.print("Enter your account number: ");
+				int number = Integer.parseInt(br.readLine());
+				for (int i = 0; i < peopleList.length; i++) {
+					if (number == peopleList[i].banknumber) {
+						System.out.print("Enter the amount to withdraw: ");
+						int amount = Integer.parseInt(br.readLine());
+						cashout(amount, number, peopleList[i].money);
+						run();
+				} else {
+					continue;
+				}
+				}
 			} catch (Exception e) {
 				break;
 			}
 		}
 	}
 
-	public void cashout(int amount) {
-		if (amount < accountBalance) {
-			accountBalance = accountBalance - amount;
+	public void cashout(int amount, int number, int money) {
+		if (amount < ATMBalance && amount < money) {
+			ATMBalance = ATMBalance - amount;
 			System.out.println("Ok, here is your money, enjoy!");
-		} else {
+			System.out.println("This is the new Balance: "+ ATMBalance);
+		} else if (amount > ATMBalance && amount < money) {
 			System.out.println("Sorry, not enough money in the bank.");
+		} else if (amount < ATMBalance && amount > money){
+			System.out.println("Sorry, the ATM does not have enough money.");
+		} else {
+			System.out.println("Sorry, neither does the ATM nor do you have enough money.");
 		}
 
 	};
