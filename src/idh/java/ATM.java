9aa555aac01b1d4a2777fd898d90ab6ce80b2f15
felix <felix@DESKTOP-NH3PVA0>
2022-04-20 11:52:08 +0200

    Abagbae

--- src/idh/java/ATM.java
+++ src/idh/java/ATM.java
@@ -4,7 +4,7 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 
 public class ATM {
-	int accountBalance = 100;
+	int moneyATM = 10000;
 
 	/**
 	 * Main command loop of the ATM Asks the user to enter a number, and passes this
@@ -12,13 +12,32 @@ public class ATM {
 	 * produces money. If the user enters anything else than an integer number, the
 	 * loop breaks and the program exists
 	 */
+	
+	int IDindex;
+	
+	Users user1 = new Users(11, 100);
+	Users user2 = new Users(12, 400);
+	Users user3 = new Users(13, 15); 
+
+	Users userList[] = {user1, user2, user3};
+	
+	
 	public void run() {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		while (true) {
 			try {
-				System.out.print("Enter the amount to withdraw: ");
-				int amount = Integer.parseInt(br.readLine());
-				cashout(amount);
+				System.out.println("Enter ID");
+				int ID = Integer.parseInt(br.readLine());
+				IDindex = accountingIndex(ID);
+				
+				if (IDindex != -1) {System.out.print("Enter the amount to withdraw: ");
+				int amount = Integer.parseInt(br.readLine());		
+					if (amount < moneyATM) {
+						cashout(amount);
+					}
+					else {System.out.println("not enough money in the ATM");
+				}}
+				else {System.out.println("wrong ID");}
 			} catch (Exception e) {
 				break;
 			}
@@ -26,15 +45,27 @@ public class ATM {
 	}
 
 	public void cashout(int amount) {
-		if (amount < accountBalance) {
-			accountBalance = accountBalance - amount;
+
+		if (amount < userList[IDindex].getBalance() ) {
+			userList[IDindex].setBalance (userList[IDindex].getBalance() - amount);
 			System.out.println("Ok, here is your money, enjoy!");
 		} else {
 			System.out.println("Sorry, not enough money in the bank.");
 		}
+		
 
 	};
 
+	public int accountingIndex(int ID) {
+
+		for (int i = 0; i < userList.length; i++) {
+			if (ID == userList[i].getID()) {
+				return i;
+			}
+		}
+		return -1;
+	}
+	
 	/**
 	 * Launches the ATM
 	 */
