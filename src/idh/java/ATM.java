405cd8cf6b8bf7f82bd3c0127d2c190e58821400
laryrauh <laryrauh@192.168.2.105>
2022-04-22 11:31:05 +0200

    Eigene Lösung einfügen

--- src/idh/java/ATM.java
+++ src/idh/java/ATM.java
@@ -2,19 +2,24 @@ package idh.java;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.Scanner;
 
 public class ATM {
-	int accountBalance = 100;
-
+	int currentBalance = 200;
+	Scanner sc = new Scanner(System.in);
+	
 	/**
-	 * Main command loop of the ATM Asks the user to enter a number, and passes this
-	 * number to the function cashout(...) which actually does the calculation and
-	 * produces money. If the user enters anything else than an integer number, the
-	 * loop breaks and the program exists
+	 * Main command loop of the ATM
+	 * Asks the user to enter a number, and passes this number to the function cashout(...) 
+	 * which actually does the calculation and produces money.
+	 * If the user enters anything else than an integer number, the loop breaks 
+	 * and the program exists
 	 */
 	public void run() {
+		
+		
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		while (true) {
+		while(true) {
 			try {
 				System.out.print("Enter the amount to withdraw: ");
 				int amount = Integer.parseInt(br.readLine());
@@ -22,19 +27,31 @@ public class ATM {
 			} catch (Exception e) {
 				break;
 			}
+			
 		}
 	}
 
+	
+	
+	
 	public void cashout(int amount) {
-		if (amount < accountBalance) {
-			accountBalance = accountBalance - amount;
-			System.out.println("Ok, here is your money, enjoy!");
-		} else {
-			System.out.println("Sorry, not enough money in the bank.");
-		}
-
+		
+		//int withdraw = sc.nextInt();
+		if (currentBalance >= amount) {
+					
+					//the account is balanced, money can be removed 
+					currentBalance = currentBalance - amount;
+					System.out.println("Ok, here is your money, enjoy!");
+					System.out.println("Your new balance is " + currentBalance);
+				} 
+				else {
+					//show error message if there's not enough money
+					System.out.println("Sorry, not enough money in the bank.");
+				}
+				System.out.println("");
 	};
-
+	
+	
 	/**
 	 * Launches the ATM
 	 */
@@ -42,5 +59,4 @@ public class ATM {
 		ATM atm = new ATM();
 		atm.run();
 	};
-
-}
+};
